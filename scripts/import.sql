41f1f2c25dcaca9882844903b8f42407e82a555f
Ramon Santos <rsanttos22@gmail.com>
2019-06-15 11:17:47 -0300

    Configuracao inicial, csvs tratados e script para importação em banco postgres

---
+++ scripts/import.sql
@@ -0,0 +1,13 @@
+﻿COPY orcamento(mes, ano, acao_orcamentaria, programa_orcamentario, funcao, tipo_favorecido, uf, municipio, valor_transferido) 
+FROM 'path/educacao/educacao14.csv' DELIMITER ';' CSV HEADER;
+COPY orcamento(mes, ano, acao_orcamentaria, programa_orcamentario, funcao, tipo_favorecido, uf, municipio, valor_transferido) 
+FROM 'path/educacao/educacao15.csv' DELIMITER ';' CSV HEADER;
+COPY orcamento(mes, ano, acao_orcamentaria, programa_orcamentario, funcao, tipo_favorecido, uf, municipio, valor_transferido) 
+FROM 'path/educacao/educacao16.csv' DELIMITER ';' CSV HEADER;
+COPY orcamento(mes, ano, acao_orcamentaria, programa_orcamentario, funcao, tipo_favorecido, uf, municipio, valor_transferido) 
+FROM 'path/educacao/educacao17.csv' DELIMITER ';' CSV HEADER;
+COPY orcamento(mes, ano, acao_orcamentaria, programa_orcamentario, funcao, tipo_favorecido, uf, municipio, valor_transferido, grupo) 
+FROM 'path/educacao/educacao18.csv' DELIMITER ';' CSV HEADER;
+UPDATE orcamento SET grupo = 'EDUCACAO'
+
+SELECT COUNT (*) FROM orcamento
---
+++ src/main/java/br/seugoverno/seugoverno/dominio/GrupoDeGasto.java
@@ -0,0 +1,5 @@
+package br.seugoverno.seugoverno.dominio;
+
+public enum GrupoDeGasto {
+	EDUCACAO, SAUDE
+}
