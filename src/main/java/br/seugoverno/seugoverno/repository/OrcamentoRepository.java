af9b84629a3e26aff653720bde0b4682e9b9bd32
Ramon Santos <rsanttos22@gmail.com>
2019-06-15 21:44:50 -0300

    Guarda alteracao

--- src/main/java/br/seugoverno/seugoverno/repository/OrcamentoRepository.java
+++ src/main/java/br/seugoverno/seugoverno/repository/OrcamentoRepository.java
@@ -1,9 +1,17 @@
 package br.seugoverno.seugoverno.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.seugoverno.seugoverno.dominio.Orcamento;
 
 public interface OrcamentoRepository extends CrudRepository<Orcamento, Integer>{
 
+	List<String> findDistinctAno();
+
+	List<String> findDistinctMes();
+
+	List<String> findDistinctProgramaOrcamentario();
+	
 }
